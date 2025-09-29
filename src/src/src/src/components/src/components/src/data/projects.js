172b11af5d5151630c0de3744be720e5cc96b672
Keekagwen <micheala@everitt.cc>
2025-09-28 20:16:36 -0500

    Create projects.js

---
+++ src/src/src/src/components/src/components/src/data/projects.js
@@ -0,0 +1,7 @@
+export const projects = [
+  { id: "KA 2375-10", vendor: "BETTIS Asphalt", ntp: "7/15/2024", pending: 2, status: "Active" },
+  { id: "KA 2375-10", vendor: "BETTIS Asphalt", ntp: "7/15/2024", pending: 2, status: "Active" },
+  { id: "KA 2375-10", vendor: "BETTIS Asphalt", ntp: "7/15/2024", pending: 2, status: "Active" },
+  { id: "KA 2375-10", vendor: "BETTIS Asphalt", ntp: "7/15/2024", pending: 2, status: "Active" },
+  { id: "KA 2375-10", vendor: "BETTIS Asphalt", ntp: "7/15/2024", pending: 2, status: "Active" },
+];
